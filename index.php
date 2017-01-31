0deb2b28ca87323e54628d1f5ed8063f2fef0d47
QuentinR <quentin.rouhier@gmail.com>
2017-01-31 08:34:47 +0100

    créé une fonction

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function tot($chameau, $chamelle)
+{
+    return $chameau.$chamelle;
+}
+  echo tot("chaine","vianney");
+    ?>
+  </body>
+</html>
